3e0b3e3dd6863fe42cec4ed5a110cb4b5a0e6eac
Lou Huang <lou@louhuang.com>
2015-11-20 16:35:50 -0500

    Update 2015

--- js/main.js
+++ js/main.js
@@ -3,17 +3,12 @@
 (function ($) {
   var MAP // global reference to the Leaflet map object
 
-  function resizeMapWindow () {
-    var aboutBoxHeight = $('#about').outerHeight()
-    $('#map').css('top', Math.floor(aboutBoxHeight))
-  }
+  var VIZ_2014 = 'https://lou.cartodb.com/api/v2/viz/d00a8afc-752e-11e4-8ec0-0e018d66dc29/viz.json'
+  var VIZ_2015 = 'https://lou.cartodb.com/api/v2/viz/c9440e96-8fb3-11e5-861b-0ea31932ec1d/viz.json'
 
-  function inIframe () {
-    try {
-      return window.self !== window.top
-    } catch (e) {
-      return true
-    }
+  function resizeMapWindow () {
+    var aboutBoxHeight = document.getElementById('about').offsetHeight
+    document.getElementById('map').style.top = Math.floor(aboutBoxHeight).toString() + 'px'
   }
 
   function checkForInfowindowData () {
@@ -31,21 +26,26 @@
   function renderInstagram (data, callback) {
     var url = data.url
 
+    // Replace http:// links with https
+    url = url.replace(/^http:/, 'https:')
+
     if (typeof instgrm === 'undefined') {
       renderInfowindowError('Unable to connect to Instagram.')
       return
     }
 
     $.ajax({
-      url: 'http://api.instagram.com/oembed?url=' + url + '&beta=true&omitscript=true',
+      url: 'https://api.instagram.com/oembed?url=' + url + '&beta=true&omitscript=true',
       dataType: 'jsonp',
       cache: false,
       success: function (response) {
+        var embedEl = document.getElementById('embedded-content')
+
         if (response.html) {
-          $('#embedded-content').html(response.html)
+          embedEl.innerHTML = response.html
           instgrm.Embeds.process()
         } else {
-          $('#embedded-content').html('<div class="error">Could not embed the instagram</div>')
+          embedEl.innerHTML = '<div class="error">Could not embed the instagram</div>'
         }
 
         if (typeof callback === 'function') callback()
@@ -61,24 +61,30 @@
     var username = data.username
     var identifier = data.identifier
 
+    var embedEl = document.getElementById('embedded-content')
+
     if (typeof twttr === 'undefined') {
       renderInfowindowError('Unable to connect to Twitter.')
       return
     }
 
     // Set the bare minimum HTML required for Twitter to render a widget
-    $('#embedded-content').html('<blockquote class="twitter-tweet" lang="en"><a href="https://twitter.com/' + username + '/status/' + identifier + '"></a></blockquote>').show()
+    embedEl.innerHTML = '<blockquote class="twitter-tweet" lang="en"><a href="https://twitter.com/' + username + '/status/' + identifier + '"></a></blockquote>'
+    embedEl.style.display = 'block'
 
     // Tell twitter to do its job
-    twttr.widgets.load(document.getElementById('embedded-content'))
+    twttr.widgets.load(embedEl)
     twttr.events.bind('rendered', function (event) {
       if (typeof callback === 'function') callback()
     })
   }
 
   function renderInfowindowError (message) {
+    var embedEl = document.getElementById('embedded-content')
+
     if (!message) message = 'There was an error loading this, please try again.'
-    $('#embedded-content').html('<div class="error">' + message + '</div>')
+
+    embedEl.innerHTML = '<div class="error">' + message + '</div>'
     afterInfowindowRender()
   }
 
@@ -107,39 +113,36 @@
     }
   }
 
-  $(document).ready(function () {
+  if (window.self === window.top) {
+    // Show the about box if we're not in an iframe
+    document.getElementById('about').style.display = 'block'
 
-    if (inIframe()) {
-      // Hide the about box if we're inside an iframe
-      $('#about').hide()
-    } else {
-      resizeMapWindow()
-      $(window).on('resize', resizeMapWindow)
-    }
+    // Resize the map to fit
+    resizeMapWindow()
+    window.addEventListener('resize', resizeMapWindow)
+  }
 
-    // Map time
-    cartodb.createVis('map', 'http://lou.cartodb.com/api/v2/viz/d00a8afc-752e-11e4-8ec0-0e018d66dc29/viz.json')
-      .on('done', function (viz, layers) {
-        var pointLayer = layers[1]
+  // Map time
+  cartodb.createVis('map', VIZ_2015)
+    .on('done', function (viz, layers) {
+      var pointLayer = layers[1]
 
-        MAP = viz.getNativeMap()
+      MAP = viz.getNativeMap()
 
-        if (inIframe()) {
-          MAP.scrollWheelZoom.disable()
-        }
+      if (window.self !== window.top) {
+        MAP.scrollWheelZoom.disable()
+      }
 
-        pointLayer
-          .on('featureClick', function (e, latlng, pos, data) {
-            checkForInfowindowData()
-          })
-          .on('error', function (err) {
-            console.log('[CartoDB] error: ' + err)
-          })
+      pointLayer
+        .on('featureClick', function (e, latlng, pos, data) {
+          checkForInfowindowData()
+        })
+        .on('error', function (err) {
+          console.log('[CartoDB] error: ' + err)
+        })
 
-      }).on('error', function (err) {
-        console.log('[CartoDB] some error occurred: ' + err)
-      })
-
-  })
+    }).on('error', function (err) {
+      console.log('[CartoDB] some error occurred: ' + err)
+    })
 
 }(jQuery))
